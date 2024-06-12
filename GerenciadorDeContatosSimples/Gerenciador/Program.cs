4b71ada16a97fdaf776c452c7b84e7b2723f18e6
Tonho du bois <tonhodubois@gmail.com>
2024-06-11 23:58:32 -0300

    feat:

---
+++ GerenciadorDeContatosSimples/Gerenciador/Program.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+
+var contatos = new ArrayList();
+int codigo = 6;
+
+while (codigo != 0){
+  Console.WriteLine("Bem vindo a agenda, digite o código da função desejada");
+  Console.WriteLine("0: Sair\n1: Exibir contatos\n2: Adicionar novo contato\n3:Remover Contato");
+  
+}
