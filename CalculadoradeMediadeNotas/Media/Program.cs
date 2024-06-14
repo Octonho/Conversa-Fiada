97d6f1f208ee5d7f82afe00a3c80cb621c9f9b65
Tonho du bois <tonhodubois@gmail.com>
2024-06-13 23:58:29 -0300

    feat: media

---
+++ CalculadoradeMediadeNotas/Media/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Digite a quantidade de notas");
+int tamanho = Convert.ToInt32(Console.ReadLine());
+double[] notas = new double[tamanho];
+double soma = 0; 
+for (int i = 0; i < tamanho; i++){
+  Console.WriteLine("Digite a nota: ");
+  notas[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+foreach(int i in notas){
+soma += notas[i - 1];
+}
+double media = soma / tamanho;
+Console.WriteLine("Sua média: " + media);
