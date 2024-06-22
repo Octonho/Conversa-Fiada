9fd484862a6aa59f76f021e5d8db416df30d51b8
SysAdminHeal <tonhodubois@gmail.com>
2024-06-22 20:30:38 -0300

    feat: add grades input feature

--- StudentPerformanceCalculator/StudentPerformance/Program.cs
+++ StudentPerformanceCalculator/StudentPerformance/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿List<Double> grades = new List<double>();
+bool active = true;
+Console.WriteLine("Input the grades:\n");
+while (active)
+{
+var grade = Convert.ToInt32(Console.ReadLine());
+if (grade < 0 ){
+  break;
+}else{
+grades.Add(grade);
+}}
+
--- StudentPerformanceCalculator/StudentPerformance/obj/Debug/net8.0/StudentPerformance.AssemblyInfo.cs
+++ StudentPerformanceCalculator/StudentPerformance/obj/Debug/net8.0/StudentPerformance.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentPerformance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6796e05782e601004853e21afb4481466b2ede3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d434e7b8f7e8803de330819df0fa82c80c8d809")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentPerformance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentPerformance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
